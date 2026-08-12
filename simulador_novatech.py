ae10df0ba3fd164a917e4610d58bc2c8dba0f997
Anthony Juan Pablo Orellana de León <aorellanad7@miumg.edu.gt>
2026-08-12 00:29:35 -0600

    feat: add initial inventory

--- simulador_novatech.py
+++ simulador_novatech.py
@@ -2,3 +2,16 @@ import threading
 import queue
 import time
 import random
+
+# --------------------------------------------------------
+# 1. RECURSOS COMPARTIDOS Y CONFIGURACIÓN INICIAL
+# --------------------------------------------------------
+
+# Inventario inicial según los datos mínimos del escenario
+inventario = {
+    "P001": {"nombre": "Teclado mecánico", "existencia": 12},
+    "P002": {"nombre": "Mouse inalámbrico", "existencia": 18},
+    "P003": {"nombre": "Audífonos USB", "existencia": 10},
+    "P004": {"nombre": "Cámara web", "existencia": 8},
+    "P005": {"nombre": "Monitor de 24 pulgadas", "existencia": 6}
+}
